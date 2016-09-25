3dff637a4b7dbed52245467c1d608b1020b346f5
jenncasper <jenncasper@berkeley.edu>
2016-09-25 19:29:19 -0400

    Added the exercise1 database and use command at the top

--- loading_and_modelling/hive_base_ddl.sql
+++ loading_and_modelling/hive_base_ddl.sql
@@ -1,6 +1,11 @@
+-- exercise1 
+DROP DATABASE IF EXISTS exercise1;
+CREATE DATABASE exercise1;
+USE exercise;
+
 -- complicationshospital
-DROP TABLE IF EXISTS exercise1.complicationshospital;
-CREATE EXTERNAL TABLE exercise1.complicationshospital (
+DROP TABLE IF EXISTS complicationshospital;
+CREATE EXTERNAL TABLE complicationshospital (
   providerid INT,
   hospitalname STRING,
   address STRING,
@@ -24,13 +29,13 @@ ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
 WITH SERDEPROPERTIES ("separatorChar" = ",", "quoteChar" = '"', "escapeChar" = '\\')
 STORED AS TEXTFILE
 tblproperties("skip.header.line.count"="1");
-LOAD DATA INPATH '/user/w205/hospital_compare/ComplicationsHospital.csv.gz' OVERWRITE INTO TABLE exercise1.complicationshospital;
--- select count(*) from exercise1.complicationshospital;
+LOAD DATA INPATH '/user/w205/hospital_compare/ComplicationsHospital.csv.gz' OVERWRITE INTO TABLE complicationshospital;
+-- select count(*) from complicationshospital;
 -- 32721
 
 -- complicationsnational
-DROP TABLE IF EXISTS exercise1.complicationsnational;
-CREATE EXTERNAL TABLE exercise1.complicationsnational (
+DROP TABLE IF EXISTS complicationsnational;
+CREATE EXTERNAL TABLE complicationsnational (
   measurename STRING,
   measureid STRING,
   nationalrate DECIMAL,
@@ -46,13 +51,13 @@ ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
 WITH SERDEPROPERTIES ("separatorChar" = ",", "quoteChar" = '"', "escapeChar" = '\\')
 STORED AS TEXTFILE
 tblproperties("skip.header.line.count"="1");
-LOAD DATA INPATH '/user/w205/hospital_compare/ComplicationsNational.csv.gz' OVERWRITE INTO TABLE exercise1.complicationsnational;
--- select count(*) from exercise1.complicationshospital;
+LOAD DATA INPATH '/user/w205/hospital_compare/ComplicationsNational.csv.gz' OVERWRITE INTO TABLE complicationsnational;
+-- select count(*) from complicationshospital;
 -- 7
 
 -- complicationsstate
-DROP TABLE IF EXISTS exercise1.complicationsstate;
-CREATE EXTERNAL TABLE exercise1.complicationsstate (
+DROP TABLE IF EXISTS complicationsstate;
+CREATE EXTERNAL TABLE complicationsstate (
   state STRING,
   measurename STRING,
   measureid STRING,
@@ -68,13 +73,13 @@ ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
 WITH SERDEPROPERTIES ("separatorChar" = ",", "quoteChar" = '"', "escapeChar" = '\\')
 STORED AS TEXTFILE
 tblproperties("skip.header.line.count"="1");
-LOAD DATA INPATH '/user/w205/hospital_compare/ComplicationsState.csv.gz' OVERWRITE INTO TABLE exercise1.complicationsstate;
--- select count(*) from exercise1.complicationshospital;
+LOAD DATA INPATH '/user/w205/hospital_compare/ComplicationsState.csv.gz' OVERWRITE INTO TABLE complicationsstate;
+-- select count(*) from complicationshospital;
 -- 392
 
 -- hcahpshospital
-DROP TABLE IF EXISTS exercise1.hcahpshospital;
-CREATE EXTERNAL TABLE exercise1.hcahpshospital (
+DROP TABLE IF EXISTS hcahpshospital;
+CREATE EXTERNAL TABLE hcahpshospital (
   providerid STRING,
   hospitalname STRING,
   address STRING,
@@ -101,13 +106,13 @@ ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
 WITH SERDEPROPERTIES ("separatorChar" = ",", "quoteChar" = '"', "escapeChar" = '\\')
 STORED AS TEXTFILE
 tblproperties("skip.header.line.count"="1");
-LOAD DATA INPATH '/user/w205/hospital_compare/HCAHPSHospital.csv.gz' OVERWRITE INTO TABLE exercise1.hcahpshospital;
--- select count(*) from exercise1.hcahpshospital;
+LOAD DATA INPATH '/user/w205/hospital_compare/HCAHPSHospital.csv.gz' OVERWRITE INTO TABLE hcahpshospital;
+-- select count(*) from hcahpshospital;
 -- 204,864
 
 -- HCAHPS National
-DROP TABLE IF EXISTS exercise1.hcahpsmational;
-CREATE EXTERNAL TABLE exercise1.hcahpsnational (
+DROP TABLE IF EXISTS hcahpsmational;
+CREATE EXTERNAL TABLE hcahpsnational (
   hcahpsmeasureid STRING,
   hcahpsquestion STRING,
   hcahpsanswerdesc STRING,
@@ -120,13 +125,13 @@ ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
 WITH SERDEPROPERTIES ("separatorChar" = ",", "quoteChar" = '"', "escapeChar" = '\\')
 STORED AS TEXTFILE
 tblproperties("skip.header.line.count"="1");
-LOAD DATA INPATH '/user/w205/hospital_compare/HCAHPSNational.csv.gz' OVERWRITE INTO TABLE exercise1.hcahpsnational;
--- select count(*) from exercise1.hcahpsnational;
+LOAD DATA INPATH '/user/w205/hospital_compare/HCAHPSNational.csv.gz' OVERWRITE INTO TABLE hcahpsnational;
+-- select count(*) from hcahpsnational;
 -- 32
 
 -- hcahpsstate
-DROP TABLE IF EXISTS exercise1.hcahpsstate;
-CREATE EXTERNAL TABLE exercise1.hcahpsstate (
+DROP TABLE IF EXISTS hcahpsstate;
+CREATE EXTERNAL TABLE hcahpsstate (
   state STRING,
   hcahpsquestion STRING,
   hcahpsmeasureid STRING,
@@ -140,13 +145,13 @@ ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
 WITH SERDEPROPERTIES ("separatorChar" = ",", "quoteChar" = '"', "escapeChar" = '\\')
 STORED AS TEXTFILE
 tblproperties("skip.header.line.count"="1");
-LOAD DATA INPATH '/user/w205/hospital_compare/HCAHPSState.csv.gz' OVERWRITE INTO TABLE exercise1.hcahpsstate;
--- select count(*) from exercise1.hcahpsstate;
+LOAD DATA INPATH '/user/w205/hospital_compare/HCAHPSState.csv.gz' OVERWRITE INTO TABLE hcahpsstate;
+-- select count(*) from hcahpsstate;
 -- 1792
 
 -- hospitalgeneralinfo
-DROP TABLE IF EXISTS exercise1.hospitalgeneralinfo;
-CREATE EXTERNAL TABLE exercise1.hospitalgeneralinfo (
+DROP TABLE IF EXISTS hospitalgeneralinfo;
+CREATE EXTERNAL TABLE hospitalgeneralinfo (
   providerid INT,
   hospitalname STRING,
   address STRING,
@@ -163,13 +168,13 @@ ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
 WITH SERDEPROPERTIES ("separatorChar" = ",", "quoteChar" = '"', "escapeChar" = '\\')
 STORED AS TEXTFILE
 tblproperties("skip.header.line.count"="1");
-LOAD DATA INPATH '/user/w205/hospital_compare/HospitalGeneralInformation.csv.gz' OVERWRITE INTO TABLE exercise1.hospitalgeneralinfo;
--- select count(*) from exercise1.hospitalgeneralinfo;
+LOAD DATA INPATH '/user/w205/hospital_compare/HospitalGeneralInformation.csv.gz' OVERWRITE INTO TABLE hospitalgeneralinfo;
+-- select count(*) from hospitalgeneralinfo;
 -- 4824
 
 -- measuredates
-DROP TABLE IF EXISTS exercise1.measuredates;
-CREATE EXTERNAL TABLE exercise1.measuredates (
+DROP TABLE IF EXISTS measuredates;
+CREATE EXTERNAL TABLE measuredates (
   measurename STRING,
   measureid STRING,
   measurestartquarter STRING,
@@ -181,13 +186,13 @@ ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
 WITH SERDEPROPERTIES ("separatorChar" = ",", "quoteChar" = '"', "escapeChar" = '\\')
 STORED AS TEXTFILE
 tblproperties("skip.header.line.count"="1");
-LOAD DATA INPATH '/user/w205/hospital_compare/MeasureDates.csv.gz' OVERWRITE INTO TABLE exercise1.measuredates;
--- select count(*) from exercise1.measuredates;
+LOAD DATA INPATH '/user/w205/hospital_compare/MeasureDates.csv.gz' OVERWRITE INTO TABLE measuredates;
+-- select count(*) from measuredates;
 -- 100
 
 -- readmissionsanddeathshospital
-DROP TABLE IF EXISTS exercise1.readmissionsanddeathshospital;
-CREATE EXTERNAL TABLE exercise1.readmissionsanddeathshospital (
+DROP TABLE IF EXISTS readmissionsanddeathshospital;
+CREATE EXTERNAL TABLE readmissionsanddeathshospital (
   providerid INT,
   hospitalname STRING,
   address STRING,
@@ -211,13 +216,13 @@ ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
 WITH SERDEPROPERTIES ("separatorChar" = ",", "quoteChar" = '"', "escapeChar" = '\\')
 STORED AS TEXTFILE
 tblproperties("skip.header.line.count"="1");
-LOAD DATA INPATH '/user/w205/hospital_compare/ReadmissionsandDeathsHospital.csv.gz' OVERWRITE INTO TABLE exercise1.readmissionsanddeathshospital;
--- select count(*) from exercise1.readmissionsanddeathshospital;
+LOAD DATA INPATH '/user/w205/hospital_compare/ReadmissionsandDeathsHospital.csv.gz' OVERWRITE INTO TABLE readmissionsanddeathshospital;
+-- select count(*) from readmissionsanddeathshospital;
 -- 66,990
 
 -- readmissionsanddeathsnational
-DROP TABLE IF EXISTS exercise1.readmissionsanddeathsnational;
-CREATE EXTERNAL TABLE exercise1.readmissionsanddeathsnational (
+DROP TABLE IF EXISTS readmissionsanddeathsnational;
+CREATE EXTERNAL TABLE readmissionsanddeathsnational (
   measurename STRING,
   measureid STRING,
   nationalrate DECIMAL,
@@ -233,13 +238,13 @@ ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
 WITH SERDEPROPERTIES ("separatorChar" = ",", "quoteChar" = '"', "escapeChar" = '\\')
 STORED AS TEXTFILE
 tblproperties("skip.header.line.count"="1");
-LOAD DATA INPATH '/user/w205/hospital_compare/ReadmissionsandDeathsNational.csv.gz' OVERWRITE INTO TABLE exercise1.readmissionsanddeathsnational;
--- select count(*) from exercise1.readmissionsanddeathsnational;
+LOAD DATA INPATH '/user/w205/hospital_compare/ReadmissionsandDeathsNational.csv.gz' OVERWRITE INTO TABLE readmissionsanddeathsnational;
+-- select count(*) from readmissionsanddeathsnational;
 -- 14
 
 -- readmissionsanddeathsstate
-DROP TABLE IF EXISTS exercise1.readmissionsanddeathsstate;
-CREATE EXTERNAL TABLE exercise1.readmissionsanddeathsstate (
+DROP TABLE IF EXISTS readmissionsanddeathsstate;
+CREATE EXTERNAL TABLE readmissionsanddeathsstate (
   state STRING,
   measurename STRING,
   measureid STRING,
@@ -255,13 +260,13 @@ ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
 WITH SERDEPROPERTIES ("separatorChar" = ",", "quoteChar" = '"', "escapeChar" = '\\')
 STORED AS TEXTFILE
 tblproperties("skip.header.line.count"="1");
-LOAD DATA INPATH '/user/w205/hospital_compare/ReadmissionsandDeathsState.csv.gz' OVERWRITE INTO TABLE exercise1.readmissionsanddeathsstate;
--- select count(*) from exercise1.readmissionsanddeathsstate;
+LOAD DATA INPATH '/user/w205/hospital_compare/ReadmissionsandDeathsState.csv.gz' OVERWRITE INTO TABLE readmissionsanddeathsstate;
+-- select count(*) from readmissionsanddeathsstate;
 -- 784
 
 -- timelyandeffectivecarehospital
-DROP TABLE IF EXISTS exercise1.timelyandeffectivecarehospital;
-CREATE EXTERNAL TABLE exercise1.timelyandeffectivecarehospital (
+DROP TABLE IF EXISTS timelyandeffectivecarehospital;
+CREATE EXTERNAL TABLE timelyandeffectivecarehospital (
   providerid STRING,
   hospitalname STRING,
   address STRING,
@@ -283,13 +288,13 @@ ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
 WITH SERDEPROPERTIES ("separatorChar" = ",", "quoteChar" = '"', "escapeChar" = '\\')
 STORED AS TEXTFILE
 tblproperties("skip.header.line.count"="1");
-LOAD DATA INPATH '/user/w205/hospital_compare/TimelyandEffectiveCareHospital.csv.gz' OVERWRITE INTO TABLE exercise1.timelyandeffectivecarehospital;
--- select count(*) from exercise1.timelyandeffectivecarehospital;
+LOAD DATA INPATH '/user/w205/hospital_compare/TimelyandEffectiveCareHospital.csv.gz' OVERWRITE INTO TABLE timelyandeffectivecarehospital;
+-- select count(*) from timelyandeffectivecarehospital;
 -- 217,821
 
 -- timelyandeffectivecarenational
-DROP TABLE IF EXISTS exercise1.timelyandeffectivecarenational;
-CREATE EXTERNAL TABLE exercise1.timelyandeffectivecarenational (
+DROP TABLE IF EXISTS timelyandeffectivecarenational;
+CREATE EXTERNAL TABLE timelyandeffectivecarenational (
   measurename STRING,
   measureid STRING,
   condition STRING,
@@ -303,13 +308,13 @@ ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
 WITH SERDEPROPERTIES ("separatorChar" = ",", "quoteChar" = '"', "escapeChar" = '\\')
 STORED AS TEXTFILE
 tblproperties("skip.header.line.count"="1");
-LOAD DATA INPATH '/user/w205/hospital_compare/TimelyandEffectiveCareNational.csv.gz' OVERWRITE INTO TABLE exercise1.timelyandeffectivecarenational;
--- select count(*) from exercise1.timelyandeffectivecarenational;
+LOAD DATA INPATH '/user/w205/hospital_compare/TimelyandEffectiveCareNational.csv.gz' OVERWRITE INTO TABLE timelyandeffectivecarenational;
+-- select count(*) from timelyandeffectivecarenational;
 -- 78
 
 -- timelyandeffectivecarestate
-DROP TABLE IF EXISTS exercise1.timelyandeffectivecarestate;
-CREATE EXTERNAL TABLE exercise1.timelyandeffectivecarestate (
+DROP TABLE IF EXISTS timelyandeffectivecarestate;
+CREATE EXTERNAL TABLE timelyandeffectivecarestate (
   state STRING,
   condition STRING,
   measurename STRING,
@@ -323,6 +328,6 @@ ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
 WITH SERDEPROPERTIES ("separatorChar" = ",", "quoteChar" = '"', "escapeChar" = '\\')
 STORED AS TEXTFILE
 tblproperties("skip.header.line.count"="1");
-LOAD DATA INPATH '/user/w205/hospital_compare/TimelyandEffectiveCareState.csv.gz' OVERWRITE INTO TABLE exercise1.timelyandeffectivecarestate;
--- select count(*) from exercise1.timelyandeffectivecarestate;
+LOAD DATA INPATH '/user/w205/hospital_compare/TimelyandEffectiveCareState.csv.gz' OVERWRITE INTO TABLE timelyandeffectivecarestate;
+-- select count(*) from timelyandeffectivecarestate;
 -- 3827
